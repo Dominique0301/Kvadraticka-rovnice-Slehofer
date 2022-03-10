abf677876d3912725b28eeeca648c37ee0ebd911
Dominik Slehofer <93545274+Dominique0301@users.noreply.github.com>
2022-03-10 13:53:57 +0100

    Final
    Kompletní program

--- main.py
+++ main.py
@@ -25,75 +25,78 @@ class Application(tk.Tk):
         self.title(self.name)
         self.bind("<Escape>", self.quit)
 
-        self.fr = tk.Frame(self, highlightbackground="blue", highlightthickness=2)
-        self.fr.grid(row=1, column=1)
+        self.fr = tk.LabelFrame(self, text="Výpočty", bd=4)
+        self.fr.grid(row=1, column=2)
+
+        self.fr2 = tk.LabelFrame(self, text="Graf", bd=4)
+        self.fr2.grid(row=1, column=4)
         
 
         #vypsání diskriminantu
         self.d=None
         self.w = 1
-        self.lbl = tk.Label(self, text="")
+        self.lbl = tk.Label(self.fr, text="")
         self.lbl.grid(row=10, column=2)
-        self.lbl_d = tk.Label(self, text="D =")
+        self.lbl_d = tk.Label(self.fr, text="D =")
         self.lbl_d.grid(row=10, column=1)
 
        #vypsání výsledků 
-        self.lblx1 = tk.Label(self, text="")
+        self.lblx1 = tk.Label(self.fr, text="")
         self.lblx1.grid(row=11,column=2)
-        self.lblx2 = tk.Label(self, text="")
+        self.lblx2 = tk.Label(self.fr, text="")
         self.lblx2.grid(row=12,column=2)
-        self.lbl_x1 = tk.Label(self, text="X1 =")
+        self.lbl_x1 = tk.Label(self.fr, text="X1 =")
         self.lbl_x1.grid(row=11, column=1)
-        self.lbl_x2 = tk.Label(self, text="X2 =")
+        self.lbl_x2 = tk.Label(self.fr, text="X2 =")
         self.lbl_x2.grid(row=12, column=1)
 
         #vstup a
-        self.lbla = tk.Label(self, text="a =")
+        self.lbla = tk.Label(self.fr, text="a =")
         self.lbla.grid(row=2, column=1)  
-        self.a_vstup = tk.Entry(self,validate="key", validatecommand=(self.register(self.validate), "%P"))
+        self.a_vstup = tk.Entry(self.fr,validate="key", validatecommand=(self.register(self.validate), "%P"))
         self.a_vstup.grid(row=2, column=2, padx=10, pady=10)
 
         #vstup b
-        self.lblb = tk.Label(self, text="b =")
+        self.lblb = tk.Label(self.fr, text="b =")
         self.lblb.grid(row=4, column=1)  
-        self.b_vstup = tk.Entry(self,validate="key", validatecommand=(self.register(self.validate), "%P"))
+        self.b_vstup = tk.Entry(self.fr,validate="key", validatecommand=(self.register(self.validate), "%P"))
         self.b_vstup.grid(row=4, column=2, padx=10, pady=10)
 
         #vstup c
-        self.lblc = tk.Label(self, text="c =")
+        self.lblc = tk.Label(self.fr, text="c =")
         self.lblc.grid(row=6, column=1)  
-        self.c_vstup = tk.Entry(self, validate="key", validatecommand=(self.register(self.validate), "%P"))
+        self.c_vstup = tk.Entry(self.fr, validate="key", validatecommand=(self.register(self.validate), "%P"))
         self.c_vstup.grid(row=6, column=2, padx=10, pady=10)
 
-        self.lblGraf=tk.Button(self, text="Zobrazit graf",command=self.linew)
+        self.lblGraf=tk.Button(self.fr2, text="Zobrazit graf",command=self.graf)
         self.lblGraf.grid(row=8,column=3)
-        self.btn=tk.Button(self, text="Diskriminant", command=self.validace)
+        self.btn=tk.Button(self.fr, text="Diskriminant", command=self.validace)
         self.btn.grid(row=8,column=2)
-        self.btn2=tk.Button(self, text="Výsledky", command=self.validace_x)
+        self.btn2=tk.Button(self.fr, text="Výsledky", command=self.validace_x)
         self.btn2.grid(row=9, column=2)
 
         #nastavení pro graf
         self.mr = IntVar()
-        self.m = tk.Checkbutton(self, text="Mřížka grafu", variable=self.mr)
+        self.m = tk.Checkbutton(self.fr2, text="Mřížka grafu", variable=self.mr)
         self.m.grid(row=2, column=4)
-        self.x_label = tk.Entry(self)
-        self.x_label.grid(row=3, column=4)
-        self.y_label = tk.Entry(self)
-        self.y_label.grid(row=4, column=4)
-        self.xlbl = tk.Label(self, text="Popis osy X")
+        self.x_label = tk.Entry(self.fr2)
+        self.x_label.grid(row=3, column=4, padx=10, pady=10)
+        self.y_label = tk.Entry(self.fr2)
+        self.y_label.grid(row=4, column=4,padx=10, pady=10)
+        self.xlbl = tk.Label(self.fr2, text="Popis osy X")
         self.xlbl.grid(row=3, column=3)
-        self.ylbl = tk.Label(self, text="Popis osy Y")
+        self.ylbl = tk.Label(self.fr2, text="Popis osy Y")
         self.ylbl.grid(row=4, column=3)
-        self.title = tk.Entry(self)
-        self.title.grid(row=5,column=4)
-        self.title_lbl = tk.Label(self, text="Název grafu")
+        self.title = tk.Entry(self.fr2)
+        self.title.grid(row=5,column=4,padx=10, pady=10)
+        self.title_lbl = tk.Label(self.fr2, text="Název grafu")
         self.title_lbl.grid(row=5, column=3)
-        self.w = tk.Entry(self, validate="key", validatecommand=(self.register(self.kontrola_tloustky),"%P"))
-        self.w.grid(row=6, column=4)
-        self.wlbl=tk.Label(self, text="Tloušťka grafu")
+        self.w = tk.Entry(self.fr2, validate="key", validatecommand=(self.register(self.kontrola_tloustky),"%P"))
+        self.w.grid(row=6, column=4,padx=10, pady=10)
+        self.wlbl=tk.Label(self.fr2, text="Tloušťka grafu")
         self.wlbl.grid(row=6, column=3)
 
-        self.geometry("600x500")
+        self.geometry("430x500")
 
 #možnost zadání desetinných čísel
     def desetinne(self, value):
@@ -123,10 +126,6 @@ class Application(tk.Tk):
         else:
             return False
         
-    
-
-   
-
     def diskriminant(self):
        self.d = (float(self.b_vstup.get())**2) - (4*float(self.a_vstup.get())*float(self.c_vstup.get()))
        self.lbl.config(text=self.d)
@@ -164,16 +163,14 @@ class Application(tk.Tk):
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
         self.x = np.linspace(self.res2, self.res1, 100)
-       #self.y1 = self.res1**2
         self.y = (float(self.a_vstup.get())*(self.x**2))+(float(self.b_vstup.get())*self.x)+float(self.c_vstup.get())
-        plt.plot(self.x, self.y, color=self.c, linewidth=self.w.get())
+        if self.w.get()=="":
+            plt.plot(self.x, self.y, color=self.c)
+        else:
+             plt.plot(self.x, self.y, color=self.c, linewidth=self.w.get())
         plt.xlabel(self.x_label.get(), loc="right")
         plt.ylabel(self.y_label.get(), loc="top")
         plt.title(self.title.get())
-        #plt.xlim(-self.res1,self.res2)
-        #plt.ylim(-self.y,self.y)
-        #plt.annotate("X1",(self.res2,self.y1))
-        #plt.annotate("x2", (self.res1, self.y1))
         ax.spines['left'].set_position('center')
         ax.spines['bottom'].set_position('center')
         ax.spines['right'].set_color('none')
@@ -189,13 +186,12 @@ class Application(tk.Tk):
 
     def graf(self):
         if self.d == None:
-             messagebox.showerror("Chyba", "Nelze zobrazit graf")
+             messagebox.showerror("Chyba", "Nemáte výsldky, které lze zobrazit")
         elif self.d > 0:
             self.graf_dve()
         else:
             pass
-        
-          
+              
 
 #validace pro počet diskriminantu
     def validace(self):
@@ -209,16 +205,6 @@ class Application(tk.Tk):
             messagebox.showerror("Chyba", "Nemáte vypočítaný diskriminant")
         else:
             self.vysledek()
-
-    def linew(self):
-        if self.w.get()=="":
-            messagebox.showwarning("Pozor", "Musíte zadat tloušťku čáry grafu.")
-        else:
-            self.graf()
-
-
-
-
 
 
 app = Application()
